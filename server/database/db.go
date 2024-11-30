8cc0e55ffd95ca31ca5c06db26ecec7ef495ff17
Yash Sharma <yashsharma2572@gmail.com>
2024-11-30 22:29:18 +0530

    Use env vars for bucket connection
    Signed-off-by: Yash Sharma <yashsharma2572@gmail.com>

--- server/database/db.go
+++ server/database/db.go
@@ -3,6 +3,8 @@ package database
 import (
 	"context"
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
@@ -18,34 +20,17 @@ type MinIOClient struct {
 func NewMinIOClient() (*MinIOClient, error) {
 	fmt.Println("Database initializing...")
 
-	// endpoint := os.Getenv("MINIO_ENDPOINT")    // E.g., "localhost:9000"
-	// accessKey := os.Getenv("MINIO_ACCESS_KEY") // E.g., "minioaccesskey"
-	// secretKey := os.Getenv("MINIO_SECRET_KEY") // E.g., "miniosecretkey"
-	// bucket := os.Getenv("MINIO_BUCKET")        // E.g., "my-bucket"
+	endpoint := os.Getenv("MINIO_ENDPOINT")
+	accessKey := os.Getenv("MINIO_ROOT_USER")
+	secretKey := os.Getenv("MINIO_ROOT_PASSWORD")
+	bucket := os.Getenv("MINIO_BUCKET")
+	useSSL := os.Getenv("MINIO_USE_SSL")
 
-	/**
-	* Test playground provided by minIO
-	 */
-	// endpoint := "play.min.io"
-	// accessKey := "Q3AM3UQ867SPQQA43P2F"
-	// secretKey := "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"
-	// useSSL := true
-	// bucket := "testbucket"
-	// MINIO_ENDPOINT="localhost:9000"
-	// MINIO_PORT= 9000
-	// MINIO_ACCESSKEY="AKIAIOSFODNN7EXAMPLE"
-	// MINIO_SECRETKEY="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-	// MINIO_BUCKET="dev-minio"
-	endpoint := "localhost:9000"
-	accessKey := "AKIAIOSFODNN7EXAMPLE"
-	secretKey := "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-	useSSL := false
-	bucket := "testbucket"
-
+	ssl, _ := strconv.ParseBool(useSSL)
 	// Initialize MinIO client
 	client, err := minio.New(endpoint, &minio.Options{
 		Creds:  credentials.NewStaticV4(accessKey, secretKey, ""),
-		Secure: useSSL, // Set to true because using HTTPS
+		Secure: ssl, // Set to true because using HTTPS
 	})
 	if err != nil {
 		return nil, fmt.Errorf("could not create MinIO client: %v", err)
